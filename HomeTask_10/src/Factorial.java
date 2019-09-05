609da0a58aebaed9b49ffeb660bf63630ad805fb
Jeka Novikov <Rul92@mail.ru>
2019-09-06 01:41:32 +0300

    fixed

--- HomeTask_10/src/Factorial.java
+++ HomeTask_10/src/Factorial.java
@@ -7,14 +7,15 @@
 public class Factorial {
     public static void main(String[] args) {
         int fact = 1;
-        int count = 10;
+        int numb = 10;
+        int count = 1;
         do {
             fact *= count;
             count++;
         }
-        while (count<=15);
+        while (count <= numb);
         System.out.println(fact);
-         /*for (int i = 10; i <= 15 ; i++) {
+        /*for (int i = 1; i <= numb ; i++) {
             fact *= i;
         }
         System.out.println(fact);*/
