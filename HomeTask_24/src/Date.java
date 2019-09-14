e2a27d57441b69bdea21964b0882c60f21e56c84
Jeka Novikov <Rul92@mail.ru>
2019-09-14 13:37:32 +0300

    Added Task_24

---
+++ HomeTask_24/src/Date.java
@@ -0,0 +1,65 @@
+//Задание 24
+//Создать класс и объекты описывающие промежуток времени. Сам промежуток
+//в классе должен задаваться тремя свойствами: секундами, минутами, часами.
+//Сделать методы для получения полного количества секунд в объекте, сравнения
+//двух объектов (метод должен работать аналогично compareTo в строках). Создать
+//два конструктора: получающий общее количество секунд, и часы, минуты и секунды
+//по отдельности. Сделать метод для вывода данных.
+
+public class Date {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    Date(int seconds){
+        this.seconds = seconds;
+    }
+
+    Date(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    @Override
+    public String toString() {
+        return "Date[" + "hours=" + hours + ", minutes=" + minutes + ", seconds=" + seconds + ']';
+    }
+
+    int getOverageSeconds () {
+        return ((hours*60*60) + (minutes*60) + seconds);
+    }
+
+    static void compareDate(Date d1, Date d2) {
+        if (d1.getOverageSeconds() > d2.getOverageSeconds()){
+            System.out.println(d1 + " больше, чем " + d2);
+        } else {
+            System.out.println(d2 + " больше, чем " + d1);
+        }
+    }
+}
---
+++ HomeTask_24/src/Runner.java
@@ -0,0 +1,18 @@
+
+
+
+
+public class Runner
+{
+    public static void main(String[] args) {
+        Date d1 = new Date(7200);
+        Date d2 = new Date(2, 35, 20);
+
+        System.out.println(d1.getOverageSeconds());
+        System.out.println(d2.getOverageSeconds());
+
+        Date.compareDate(d1, d2);
+    }
+
+
+}
