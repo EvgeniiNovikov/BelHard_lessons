de48cb936b988e3fd884bc773b08f8ec54a0d526
Jeka Novikov <Rul92@mail.ru>
2019-10-07 11:46:43 +0300

    fixed. calculate the average

--- HomeTask_35/src/Runner.java
+++ HomeTask_35/src/Runner.java
@@ -3,29 +3,30 @@
 
 import java.io.*;
 
-public class Runner
-{
+public class Runner {
     public static void main(String[] args) throws IOException {
         File file = new File("binaryNumbers.dat");
-        DataOutputStream dos;
-        DataInputStream dis;
-        try {
-            dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("binaryNumbers.dat")));
+        try (DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("binaryNumbers.dat")))) {
             for (int i = 0; i < 20; i++) {
                 dos.writeInt((int) ((Math.random()) * 100));
             }
-            dos.close();
-            dis = new DataInputStream(new BufferedInputStream(new FileInputStream("binaryNumbers.dat")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try (DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream("binaryNumbers.dat")))) {
             int num = dis.readInt();
-            while (true){
+            int a = 0;
+            while (true) {
                 System.out.print(num + " ");
+                a += num;
                 try {
                     num = dis.readInt();
-                } catch (EOFException e){
+                } catch (EOFException e) {
                     break;
                 }
             }
-            dis.close();
+            System.out.println();
+            System.out.println("Среднее арифметическое: " + a / 20);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
