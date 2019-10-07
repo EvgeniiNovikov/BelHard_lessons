f1f9bb96b0eee2de7ab3873416f498837a81a819
Jeka Novikov <Rul92@mail.ru>
2019-10-08 02:11:23 +0300

    Add words counting

--- HomeTask_33/src/TestReader.java
+++ HomeTask_33/src/TestReader.java
@@ -9,21 +9,26 @@ import java.io.IOException;
 public class TestReader {
     public static void main(String[] args) throws IOException {
         FileReader fileReader = null;
+        String s = "";
+        String[] a;
         try {
             fileReader = new FileReader("test.txt");
             int res = fileReader.read();
             int count = 0;
             while (res != -1) {
+                s += (char) res;
                 if ((char) res == ',' || (char) res == '.' || (char) res == ':' || (char) res == '!' || (char) res == ';' || (char) res == '?') {
                     count++;
                 }
                 res = fileReader.read();
             }
+            a = s.trim().split("[[,;:.!?\\]\\s]]+");
             System.out.println("Punct counter: " + count);
+            System.out.println("Количество слов: " + a.length);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
-            if(fileReader == null){
+            if (fileReader == null) {
                 System.out.println("Reader not initialized");
             } else {
                 fileReader.close();
