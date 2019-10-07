1293a6c02d9698fc6415aa8fcac1242d476da477
Jeka Novikov <Rul92@mail.ru>
2019-10-07 11:43:52 +0300

    fixed. All lines are read

--- HomeTask_34/src/Runner.java
+++ HomeTask_34/src/Runner.java
@@ -4,6 +4,8 @@
 //распечатать.
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,50 +14,36 @@ import java.util.regex.Pattern;
 
 public class Runner {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = null;
         Pattern pattern;
         Matcher matcher;
-        String text;
         List<Integer> list = new ArrayList<>();
+        String text = new String(Files.readAllBytes(Paths.get("numbersTextTest.txt")));
 
-        try {
-            reader = new BufferedReader(new FileReader("numbersTextTest.txt"));
-            text = reader.readLine();
-            pattern = Pattern.compile("\\d+");
-            matcher = pattern.matcher(text);
+        pattern = Pattern.compile("\\d+");
+        matcher = pattern.matcher(text);
 
-            while (matcher.find()) {
-                list.add(Integer.parseInt(matcher.group()));
-            }
-            System.out.print("Найденные числа: ");
-            print(list);
-            int sum = 0;
-            for (Integer i : list) {
-                sum += i;
-            }
-
-            int i = 0;
-            Collections.sort(list);
-            while (i < list.size() - 1) {
-                if (list.get(i).equals(list.get(i + 1))) {
-                    list.remove(list.get(i));
-                } else {
-                    i++;
-                }
-            }
-            System.out.println("Сумма найденых чисел из файла: " + sum);
-            System.out.print("Числа после удаления повторяшек: ");
-            print(list);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        while (matcher.find()) {
+            list.add(Integer.parseInt(matcher.group()));
         }
-        finally {
-            if(reader == null){
-                System.out.println("Reader not initialized");
+        System.out.print("Найденные числа: ");
+        print(list);
+        int sum = 0;
+        for (Integer i : list) {
+            sum += i;
+        }
+
+        int i = 0;
+        Collections.sort(list);
+        while (i < list.size() - 1) {
+            if (list.get(i).equals(list.get(i + 1))) {
+                list.remove(list.get(i));
             } else {
-                reader.close();
+                i++;
             }
         }
+        System.out.println("Сумма найденых чисел из файла: " + sum);
+        System.out.print("Числа после удаления повторяшек: ");
+        print(list);
     }
 
     public static <T> void print(List<T> list) {
